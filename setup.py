c968bd19da5a2403a5b5773075d77790522a3936
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2024-07-24 13:25:43 +0200

    Add py.typed to support PEP 561.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uschemap-py',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     package_data={'uschemap-py': ['USchemap.ecore', 'USchemap/*']},
     include_package_data=True,
